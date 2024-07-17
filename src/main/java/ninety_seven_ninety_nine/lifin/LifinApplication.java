5bf44d06adff9399605ffb7293f106119fac4328
JooIcksoo <dlrtn751@gmail.com>
2024-07-18 01:23:46 +0900

    feat(root): initial setting

---
+++ src/main/java/ninety_seven_ninety_nine/lifin/LifinApplication.java
@@ -0,0 +1,13 @@
+package ninety_seven_ninety_nine.lifin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ninety_seven_ninety_nine/lifin/LifinApplicationTests.java
@@ -0,0 +1,13 @@
+package ninety_seven_ninety_nine.lifin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
