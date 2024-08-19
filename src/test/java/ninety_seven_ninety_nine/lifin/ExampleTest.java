d68a30e1041c53335edfe413d0c4634da5f442cc
JooIcksoo <dlrtn751@gmail.com>
2024-08-19 11:57:12 +0900

    feat(test): 임시 테스트 코드 추가

---
+++ src/test/java/ninety_seven_ninety_nine/lifin/ExampleTest.java
@@ -0,0 +1,17 @@
+package ninety_seven_ninety_nine.lifin;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class ExampleTest {
+
+    @Test
+    void example() {
+        System.out.println("Hello, World!");
+
+        assertTrue(true);
+    }
+
+}
